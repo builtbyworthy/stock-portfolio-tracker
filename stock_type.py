de3e2ac5c8b0df6d9efd6b7e64c1ed81938cd952
builtbyworthy <worthyelevates@gmail.com>
2025-06-21 13:19:19 +0100

    Splitting the code for accessibility.

---
+++ stock_type.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field, field_serializer
+
+
+class StockBase(BaseModel):
+    symbol: str = Field(..., min_length=1, max_length=10,
+                        description="Stock symbol")
+    quantity: int = Field(..., ge=0,
+                          description="Stock quantity (Quantity must be non-negative)")
+
+
+class Stock(StockBase):
+    stock_id: int = Field(..., description="Unique identifier of the stock")
+
+
+class StockCreate(StockBase):
+    pass
+
+
+class StockUpdate(BaseModel):
+    symbol: Optional[str] = Field(None, min_length=1, max_length=10,
+                                  description="Stock symbol")
+    quantity: Optional[int] = Field(None, ge=0,
+                                    description="Stock quantity (Quantity must be non-negative)")
+
+
+class PortfolioStock(StockCreate):
+    price: float = Field(..., description="Stock price")
+
+    @field_serializer("price")
+    def format_price(self, value: float):
+        return f"{value:.2f}"
