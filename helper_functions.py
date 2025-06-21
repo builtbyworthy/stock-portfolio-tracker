8281f1b0801c322d672a6a40443029b691a20a6e
builtbyworthy <worthyelevates@gmail.com>
2025-06-21 13:31:26 +0100

    Adding the env variables to the get_db_connection function.

---
+++ helper_functions.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+import os
+from typing import List
+from contextlib import contextmanager
+
+from mysql import connector as mysql_connector
+from fastapi import HTTPException
+
+from stock_type import Stock
+# Database Configuration
+
+
+def get_db_connection():
+    """
+    Returns the DB Connection.
+    """
+    return mysql_connector.connect(
+        host=os.getenv("SPT_DB_HOST"),
+        user=os.getenv("SPT_DB_USER"),
+        password=os.getenv("SPT_DB_PASSWORD"),
+        database=os.getenv("SPT_DB_NAME")
+    )
+
+
+@contextmanager
+def db_cursor():
+    """
+    Yields the DB cursor, to use for querying.
+    """
+    db = get_db_connection()
+    cursor = db.cursor(dictionary=True)
+
+    try:
+        yield cursor
+    finally:
+        cursor.close()
+        db.close()
+
+
+def convert_to_stock(data: List[dict]):
+    try:
+        return [Stock(**row) for row in data]
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Data formatting error: {str(e)}")
